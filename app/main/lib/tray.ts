dfe357b9deee7fdf0394ecde9373ffecd3dfc1b1
Tom Watson <tom@tomjwatson.com>
2019-09-12 09:48:04 +0200

    use macOS tray icon

--- app/main/lib/tray.ts
+++ app/main/lib/tray.ts
@@ -11,9 +11,16 @@ let tray = null
 
 export function buildTray(): void {
   if (!tray) {
-    const imgPath = process.env.NODE_ENV === 'development' ?
-      'app/main/icon.png' :
-      path.join(process.resourcesPath, 'icon.png')
+    let imgPath
+    if (process.platform === 'darwin') {
+      imgPath = process.env.NODE_ENV === 'development' ?
+        'app/main/tray-IconTemplate.png' :
+        path.join(process.resourcesPath, 'tray-IconTemplate.png')
+    } else {
+      imgPath = process.env.NODE_ENV === 'development' ?
+        'app/main/icon.png' :
+        path.join(process.resourcesPath, 'icon.png')
+    }
     tray = new Tray(imgPath)
   }
 
