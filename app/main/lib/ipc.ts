286b30699d62f5e19b888d1f23450fc5fe04a7cb
Tom Watson <tom@tomjwatson.com>
2020-02-02 14:04:13 +0100

    use electron-log for ipc logging

--- app/main/lib/ipc.ts
+++ app/main/lib/ipc.ts
@@ -1,4 +1,5 @@
 import {ipcMain, IpcMainEvent, BrowserWindow} from 'electron'
+import log from 'electron-log'
 import {Settings} from '../../types/settings'
 import {IpcChannel} from '../../types/ipc'
 import {BreakTime} from '../../types/breaks'
@@ -9,7 +10,7 @@ import {getSettings, setSettings} from './store'
 export function sendIpc(channel: IpcChannel, ...args: any[]): void {
   const windows: BrowserWindow[] = getWindows()
 
-  console.log(`Send event ${channel}`, args)
+  log.info(`Send event ${channel}`, args)
 
   for (const window of windows) {
     if (!window) {
@@ -21,35 +22,35 @@ export function sendIpc(channel: IpcChannel, ...args: any[]): void {
 }
 
 ipcMain.on(IpcChannel.GET_SETTINGS, (event: IpcMainEvent): void => {
-  console.log(IpcChannel.GET_SETTINGS)
+  log.info(IpcChannel.GET_SETTINGS)
 
   try {
     const settings: Settings = getSettings()
     event.reply(IpcChannel.GET_SETTINGS_SUCCESS, settings)
   } catch (err)  {
-    console.error(err)
+    log.error(err)
     event.reply(IpcChannel.ERROR, err.message)
   }
 })
 
 ipcMain.on(IpcChannel.SET_SETTINGS, (event: IpcMainEvent, settings: Settings): void => {
-  console.log(IpcChannel.SET_SETTINGS, {settings})
+  log.info(IpcChannel.SET_SETTINGS, {settings})
   try {
     setSettings(settings)
     event.reply(IpcChannel.SET_SETTINGS_SUCCESS, settings)
   } catch (err)  {
-    console.error(err)
+    log.error(err)
     event.reply(IpcChannel.ERROR, err.message)
   }
 })
 
 ipcMain.on(IpcChannel.GET_BREAK_END_TIME, (event: IpcMainEvent): void => {
-  console.log(IpcChannel.GET_BREAK_END_TIME)
+  log.info(IpcChannel.GET_BREAK_END_TIME)
   try {
     const breakTime: BreakTime = getBreakEndTime()
     event.reply(IpcChannel.GET_BREAK_END_TIME_SUCCESS, breakTime ? breakTime.toISOString() : null)
   } catch (err)  {
-    console.error(err)
+    log.error(err)
     event.reply(IpcChannel.ERROR, err.message)
   }
 })
