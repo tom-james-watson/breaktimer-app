891e6d727c27b18b62e384310205cfee5690f337
Benomi Tekle <benomi@protonmail.com>
2022-01-05 14:35:08 +0300

    🐞 fix: fix autostart problem with AppImage package
    Closes #121, #133

--- app/main/lib/auto-launch.ts
+++ app/main/lib/auto-launch.ts
@@ -1,6 +1,12 @@
 import AutoLaunch from "auto-launch";
 
-const AppLauncher = new AutoLaunch({ name: "BreakTimer" });
+let app = { name: "BreakTimer" };
+
+if (process.env.APPIMAGE) {
+  app = Object.assign(app, { path: process.env.APPIMAGE })
+}
+
+const AppLauncher = new AutoLaunch(app);
 
 export function setAutoLauch(autoLaunch: boolean): void {
   if (process.env.NODE_ENV !== "development") {
